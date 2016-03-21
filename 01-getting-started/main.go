311be8b8230c4d765d0460ee9c303800c8dfd427
Kel Cecil <kelcecil@praisechaos.com>
2016-03-20 22:38:23 -0400

    Add new first item

---
+++ 01-getting-started/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Print to standard out using fmt.Printf
+	fmt.Printf(newSentence())
+}
+
+func newSentence() string {
+	// Define a "string" here
+
+	// Define a "int" or "float" here
+
+	// Define a "boolean" here
+
+	// Return a formatted string with fmt.Sprintf!
+	return fmt.Sprintf("Change this to print your variables!")
+}
