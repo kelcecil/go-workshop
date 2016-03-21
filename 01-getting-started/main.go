60e5be3183b1b6b0e1956ab2b9f0ef76fe36e102
Kel Cecil <kelcecil@praisechaos.com>
2016-03-20 22:47:13 -0400

    Make an if statement example

--- 01-getting-started/main.go
+++ 01-getting-started/main.go
@@ -3,17 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	// Print to standard out using fmt.Printf
-	fmt.Printf(newSentence())
+	// Define a "string" here with your name here
+
+	// Define an "int" as you age here.
+
+	// Define a "bool" to tell if you're a student.
+
+	// Replace these values with your variables
+	fmt.Printf(printNameAndAge("", 0))
+	fmt.Printf(printStudentStatus(true))
 }
 
-func newSentence() string {
-	// Define a "string" here
-
-	// Define a "int" or "float" here
-
-	// Define a "boolean" here
-
+func printNameAndAge(name string, age int) string {
 	// Return a formatted string with fmt.Sprintf!
-	return fmt.Sprintf("Change this to print your variables!")
+	return fmt.Sprintf("Change this to print your name and age!")
+}
+
+func printStudentStatus(student bool) string {
+	// Create an if statement to print if you're a student.
+	return fmt.Sprintf("Are you a student?")
 }
